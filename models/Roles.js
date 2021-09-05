8d4faf55204e013a3161a4302e1000be6ebdc711
Kittipat <first1801@hotmail.com>
2021-09-05 16:59:03 +0700

    add more column to role to manage role permission

--- models/Roles.js
+++ models/Roles.js
@@ -5,6 +5,11 @@ module.exports = model;
 function model(sequelize) {
     const attributes = {
         roleName: { type: DataTypes.STRING, allowNull: false },
+        users:{ type: DataTypes.STRING, allowNull: false },
+        shops:{ type: DataTypes.STRING, allowNull: false },
+        contacts:{ type: DataTypes.STRING, allowNull: false },
+        roles:{ type: DataTypes.STRING, allowNull: false }
+
     };
 
 

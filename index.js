3ff88aa2fdb7ccfdfc5f48ef32ad70578a588fe9
Kittipat <first1801@hotmail.com>
2021-10-20 18:28:08 +0700

    double quote to single quote

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const cors = require("cors");
+const cors = require('cors');
 const pool = require('./db');
 const user = require('./routes/auth');
 const shop = require('./routes/shop');
@@ -9,20 +9,22 @@ const role = require('./routes/role');
 const errorHelpers = require('./middlewares/error-handler');
 
 const app = express();
-app.use(cors({credentials: true, origin: process.env.CORSURL}));
+app.use(cors({ credentials: true, origin: process.env.CORSURL }));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cookieParser(process.env.COOKIESECRET))
-app.use("/api/user",user);
-app.use("/api/shop",shop);
-app.use("/api/role",role);
+app.use(cookieParser(process.env.COOKIESECRET));
+app.use('/api/user', user);
+app.use('/api/shop', shop);
+app.use('/api/role', role);
 
-app.get("/",async(req,res)=>{
-    res.status(200).send("sucess")
-
-})
+app.get('/', async (req, res) => {
+  res.status(200).send('sucess');
+});
 app.use(errorHelpers);
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : process.env.PORT;
+const port =
+  process.env.NODE_ENV === 'production'
+    ? process.env.PORT || 80
+    : process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}`));
